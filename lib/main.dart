9e5eb0e858d106b4b01d794eae5f35eb3ce08a51
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-09-20 19:08:37 +0330

    Save datatypes and Objects using hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/car.dart';
+import 'package:note_app/screens/home_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/student.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('names');
+
+  Hive.registerAdapter(CarAdapter());
+  await Hive.openBox<Car>('carBox');
+
+  Hive.registerAdapter(StudentAdapter());
+  await Hive.openBox<Student>('studentBox');
   runApp(const Application());
 }
 
@@ -12,12 +24,7 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-            child: Center(
-          child: Text('Initial Application'),
-        )),
-      ),
+      home: HomeScreen(),
     );
   }
 }
